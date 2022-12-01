d8944d648bbe6e023e209374e44f728f7f4efc02
DevWonny <cjfdnjs1994@naver.com>
2022-12-01 22:04:21 +0900

    feat : intro page 구현중

---
+++ components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="$emit('click', Text)">{{ Text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    Text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: 300px;
+  height: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <Tutorial/>
+  <div class="intro_wrap">
+    <Button :Text="login" @click="onClick" />
+    <Button :Text="signUp" @click="onClick" />
+  </div>
 </template>
 
 <script>
+import Button from "../components/Button.vue";
 export default {
-  name: 'IndexPage'
-}
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      login: "Login",
+      signUp: "Sign Up",
+    };
+  },
+  methods: {
+    onClick(item) {
+      console.log(`${item} 페이지로 이동!`);
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+.intro_wrap {
+  max-width: 500px;
+  height: 100vh;
+  margin: auto;
+  display: flex;
+}
+</style>
